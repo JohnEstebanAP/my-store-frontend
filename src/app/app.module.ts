d0c338d3eb1bdb9516539b2fa38fcb5d13980501
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2022-08-22 22:59:04 -0500

    add component store-home and conection routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreHomeComponent } from './store-home/store-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoreHomeComponent
   ],
   imports: [
     BrowserModule,
